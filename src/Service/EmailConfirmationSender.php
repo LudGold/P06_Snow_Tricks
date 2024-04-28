3c9bc8f3f810a0f626e162d4a501c07d261e5b17
Goldstein Ludivine <sneakymombox@gmail.com>
2024-04-28 17:53:52 +0200

    authentification reset password ok

--- src/Service/EmailConfirmationSender.php
+++ src/Service/EmailConfirmationSender.php
@@ -10,15 +10,15 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 class EmailConfirmationSender
-{  
+{
 
-     public function __construct(private MailerInterface $mailer, private UrlGeneratorInterface $urlGenerator)
+    public function __construct(private MailerInterface $mailer, private UrlGeneratorInterface $urlGenerator)
     {
     }
 
     public function sendConfirmationEmail(User $user, string $confirmationUrl): void
     {
-            
+
         $email = (new TemplatedEmail())
             ->from('noreply@example.com')
             ->to($user->getEmail())
@@ -27,7 +27,7 @@ class EmailConfirmationSender
             ->context([
                 'confirmationUrl' => $confirmationUrl,
             ]);
-    
+
         $this->mailer->send($email);
     }
     public function sendResetPasswordEmail(User $user, string $resetPasswordUrl): void
@@ -36,7 +36,7 @@ class EmailConfirmationSender
             ->from('noreply@example.com')
             ->to($user->getEmail())
             ->subject('Mot de passe oublié')
-            ->htmlTemplate('security/reset_password.html.twig')
+            ->htmlTemplate('security/new_password.html.twig')
             ->context([
                 'resetPasswordUrl' => $resetPasswordUrl,
                 'token'  => $user->getResetToken(),
@@ -44,5 +44,4 @@ class EmailConfirmationSender
 
         $this->mailer->send($email);
     }
-}     
-
+}
